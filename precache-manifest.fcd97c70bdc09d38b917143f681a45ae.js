52ec27e22813c3316581a68b7d0ccf16b58f09fb
calebjoshuapaul <calebjoshuapaul@gmail.com>
2022-04-18 11:23:08 +0530

    Updates

---
+++ precache-manifest.fcd97c70bdc09d38b917143f681a45ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58076d35dd5357fe9330",
+    "url": "http//:calebjoshuapaul.github.io/todo-app/static/css/main.154a5220.chunk.css"
+  },
+  {
+    "revision": "58076d35dd5357fe9330",
+    "url": "http//:calebjoshuapaul.github.io/todo-app/static/js/main.58076d35.chunk.js"
+  },
+  {
+    "revision": "4714a85d5d8c2c16e5d8",
+    "url": "http//:calebjoshuapaul.github.io/todo-app/static/js/1.4714a85d.chunk.js"
+  },
+  {
+    "revision": "cfa0fffe3cb87a971c21",
+    "url": "http//:calebjoshuapaul.github.io/todo-app/static/js/runtime~main.cfa0fffe.js"
+  },
+  {
+    "revision": "ccb23e51207775c4d9a44f358489d7f8",
+    "url": "http//:calebjoshuapaul.github.io/todo-app/index.html"
+  }
+];
