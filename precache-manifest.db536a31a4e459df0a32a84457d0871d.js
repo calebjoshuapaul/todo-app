9cc25ea073a5deb61b21d868a978bfe70eae9890
calebjoshuapaul <calebjoshuapaul@gmail.com>
2022-04-18 19:32:39 +0530

    Updates

---
+++ precache-manifest.db536a31a4e459df0a32a84457d0871d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d71f54ccbecf24576307",
+    "url": "/todo-app/static/css/main.b258b457.chunk.css"
+  },
+  {
+    "revision": "d71f54ccbecf24576307",
+    "url": "/todo-app/static/js/main.d71f54cc.chunk.js"
+  },
+  {
+    "revision": "4714a85d5d8c2c16e5d8",
+    "url": "/todo-app/static/js/1.4714a85d.chunk.js"
+  },
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "a8753cd928fbfbf415dcef4f0b9f9803",
+    "url": "/todo-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https//:calebjoshuapaul.github.io/todo-app/precache-manifest.16840dda1e69de443a08da30e49e2cf4.js"
+  "/todo-app/precache-manifest.db536a31a4e459df0a32a84457d0871d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https//:calebjoshuapaul.github.io/todo-app/index.html", {
+workbox.routing.registerNavigationRoute("/todo-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
